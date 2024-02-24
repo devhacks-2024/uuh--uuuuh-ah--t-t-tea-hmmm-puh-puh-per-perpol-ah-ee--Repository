ceaf3a4968430fdb2e56845f65ed1fc73bb9bcce
CarsonDDD <carson-dickinson@hotmail.com>
2024-02-23 20:24:09 -0600

    Add moddules

---
+++ Hackathon/Modules/PlayerModule.cs
@@ -0,0 +1,16 @@
+﻿using Discord.WebSocket;
+using Hackathon.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Modules;
+internal class PlayerModule : CommandModule
+{
+	public PlayerModule(ILogger<CommandModule> logger, MongoDBService mongoDbService, OpenAIService openAIService, DiscordSocketClient client, InteractionHandler interaction) : base(logger, mongoDbService, openAIService, client, interaction)
+	{
+	}
+}
