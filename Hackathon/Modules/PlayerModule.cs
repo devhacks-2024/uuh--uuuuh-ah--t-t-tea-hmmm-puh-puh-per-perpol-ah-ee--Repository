d3d030181cd8dc1451d926beb49b55609cb062c9
CarsonDDD <carson-dickinson@hotmail.com>
2024-02-23 20:37:27 -0600

    add

--- Hackathon/Modules/PlayerModule.cs
+++ Hackathon/Modules/PlayerModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.WebSocket;
+﻿using Discord.Interactions;
+using Discord.WebSocket;
 using Hackathon.Services;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Hackathon.Modules;
+
+[Group("player", "Commands assosiated with YOU!")]
 internal class PlayerModule : CommandModule
 {
 	public PlayerModule(ILogger<CommandModule> logger, MongoDBService mongoDbService, OpenAIService openAIService, DiscordSocketClient client, InteractionHandler interaction) : base(logger, mongoDbService, openAIService, client, interaction)
